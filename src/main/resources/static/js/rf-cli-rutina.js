4604f961c76a7290252f0bb1bf674675b31238c6
PC <peterpaul.0194@gmail.com>
2019-07-08 11:39:17 -0500

    git ignore modified

--- src/main/resources/static/js/rf-cli-rutina.js
+++ src/main/resources/static/js/rf-cli-rutina.js
@@ -45,6 +45,7 @@ class Rutina {
             Indicadores.instanciarKilometrajes();
             //Indicadores.instanciarPorcentajeAvance();
         }
+        $('.svg-wrap').tooltip();
 
     }
 
@@ -2654,7 +2655,7 @@ RutinaDiaHTML = (function(){
                           <div class="panel-heading">
                             <h3>${elemento.nombre}<div class="mas_menos"><img class="svg" src="img/iconos/icon_menos.svg"><img class="svg" src="img/iconos/icon_mas.svg"></div></h3>
                             <div class="icons">
-                              <div class="svg-wrap" rel="tooltip" title="x"><img class="svg" src="img/iconos/icon_microfono.svg"></div><img class="svg${elemento.nota ? ' svg-color-nota':''}" src="img/iconos/icon_leyenda.svg">
+                              <div class="svg-wrap" rel="tooltip" title="Vamos a realizar una pequeña prueba"><img class="svg" src="img/iconos/icon_microfono.svg"></div><img class="svg${elemento.nota ? ' svg-color-nota':''}" src="img/iconos/icon_leyenda.svg">
                               <span><img class="svg" src="img/iconos/icon_tiempo2.svg">${elemento.minutos}</span>
                               <a data-toggle="collapse" href="#elemento-${ix+''+diaIndex}"><img class="svg arrow" src="img/iconos/icon_flecha2.svg"></a>
                             </div>
