9bcf803fdb670cdfd3f788e1a44f756d4403a03a
PC <peterpaul.0194@gmail.com>
2019-11-05 17:51:16 -0500

    remove some email's methods than were never used

--- src/main/java/com/itsight/service/EmailService.java
+++ src/main/java/com/itsight/service/EmailService.java
@@ -2,10 +2,6 @@ package com.itsight.service;
 
 public interface EmailService {
 
-	void enviarCorreoActivarUsuario(String asunto, String receptor, String contenido);
-
-	void enviarCorreoRecuperarContrasena(String asunto, String receptor, String contenido);
-
 	void enviarCorreoInformativo(String asunto, String receptor, String contenido);
 
 	void enviarCorreoInformativoVariosBbc(String asunto, String receptores, String contenido);
